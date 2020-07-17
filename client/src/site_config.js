6c569ab0da8b42fa0e2fdb2a5efb7979054f82d3
RohitKaushal7 <rohitkk252550@gmail.com>
2020-07-17 11:57:48 +0530

    +site_config.js

---
+++ client/src/site_config.js
@@ -0,0 +1,84 @@
+exports.footer = {
+    about: [
+        {
+            tag: 'Contact Us',
+            link: '/'
+        },
+        {
+            tag: 'About Us',
+            link: '/'
+        },
+        {
+            tag: 'Careers',
+            link: '/'
+        },
+        {
+            tag: 'Laladukaan Stories',
+            link: '/'
+        },
+        {
+            tag: 'News',
+            link: '/'
+        },
+    ],
+    help: [
+        {
+            tag: 'Payments',
+            link: '/'
+        },
+        {
+            tag: 'Shipping',
+            link: '/'
+        },
+        {
+            tag: 'Cancelation & Returns',
+            link: '/'
+        },
+        {
+            tag: 'FAQ',
+            link: '/'
+        }
+    ],
+    policy: [
+        {
+            tag: 'Return Policies',
+            link: '/'
+        },
+        {
+            tag: 'Tems of Use',
+            link: '/'
+        },
+        {
+            tag: 'Security',
+            link: '/'
+        },
+        {
+            tag: 'Privacy',
+            link: '/'
+        }
+    ],
+    social: [
+        {
+            tag: 'Facebook',
+            link: '/'
+        },
+        {
+            tag: 'Twitter',
+            link: '/'
+        },
+        {
+            tag: 'Youtube',
+            link: '/'
+        },
+        {
+            tag: 'Instagram',
+            link: '/'
+        }
+    ],
+
+    address: `Laladukaan Private Limmited Clove Embassy Tech Village, Outer Ring Road, Devarabeesanahalli Village, Bengaluru, Karnataka, India`,
+
+    email: 'support.laladukaan@gmail.com',
+    telephone: '+91 9876543210'
+
+}
